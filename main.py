f31fa165d86941c997e96c219feb0983cedc681c
nyradhr <alessio.piccione@hotmail.it>
2024-12-19 23:21:38 +0100

    bookbot counts the number of words and unique characters in the book

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ def main():
     file_contents = get_book_text(book_path)
     #print(file_contents)
     print(count_words(file_contents))
+    count_unique_characters(file_contents)
 
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_unique_characters(text):
+    chars = {}
+    for char in text:
+        if char.lower() not in chars:
+            chars[char.lower()] = 1
+        else:
+            chars[char.lower()] += 1
+    print(chars)
 
 def get_book_text(path):
     with open(path) as f:
