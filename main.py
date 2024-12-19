184f65b9de54b394ff0cd1196c9b3c8b1be3b756
nyradhr <alessio.piccione@hotmail.it>
2024-12-19 23:40:49 +0100

    Bookbot creates a report detailing number of words and alphabetical characters in the book

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     file_contents = get_book_text(book_path)
     #print(file_contents)
-    print(count_words(file_contents))
-    count_unique_characters(file_contents)
+    #print(count_words(file_contents))
+    #print(count_unique_characters(file_contents))
+    get_report(book_path, file_contents)
 
 def count_words(text):
     words = text.split()
@@ -16,7 +17,17 @@ def count_unique_characters(text):
             chars[char.lower()] = 1
         else:
             chars[char.lower()] += 1
-    print(chars)
+    return chars
+
+def get_report(book_path, text):
+    words = count_words(text)
+    chars = count_unique_characters(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words} words found in the document\n")
+    for char in chars:
+        if char.isalpha():
+            print(f"The {char} character was found {chars[char]} times")
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
